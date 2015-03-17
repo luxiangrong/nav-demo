d8c1e9f7fb711c3bbafa939c21eb5f7bc60cc3c5
LuXiangrong <luxiangrongwave@gmail.com>
2015-03-17 22:36:13 +0800

    小修改

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,29 +2,30 @@
 (function($) {
     $(document).ready(function() {
         $('.secondary li').on('mouseenter', function() {
-            $(this).find('.thirdly').slideDown();
-            $(this).on('mouseleave', function() {
+            var _this = $(this);
+            _this.find('.thirdly').slideDown();
+            _this.on('mouseleave', function() {
                 $(this).find('.thirdly').slideUp();
             });
         });
 
-        $('.menu li').on('mouseenter', function(){
-        	var target = $(this).attr('data-target');
-        	$('.menu li').removeClass('active');
-        	$('.secondary-wrap[id!="' + target + '"]').slideUp();
-        	$(this).addClass('active');
-        	$('#' + target).slideDown();
+        $('.menu li').on('mouseenter', function() {
+            var target = $(this).attr('data-target');
+            $('.menu li').removeClass('active');
+            $('.secondary-wrap[id!="' + target + '"]').slideUp();
+            $(this).addClass('active');
+            $('#' + target).slideDown();
         });
 
-        $('.secondary-wrap').on('mouseleave', function(){
-        	var id = $(this).attr('id');
-        	var _this = $(this);
-        	$('.menu li').removeClass('active');
-        	$(this).slideUp();
-        	window.setTimeout(function(){
+        $('.secondary-wrap').on('mouseleave', function() {
+            var id = $(this).attr('id');
+            var _this = $(this);
+            $('.menu li').removeClass('active');
+            $(this).slideUp();
+            window.setTimeout(function() {
 
-        	}, 100);
-        	
+            }, 100);
+
         });
 
     });
