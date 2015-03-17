a52c2c84399cb16b519076c9327c005b78865aee
LuXiangrong <luxiangrongwave@gmail.com>
2015-03-17 21:28:54 +0800

    完成

---
+++ app/scripts/main.js
@@ -0,0 +1,31 @@
+'use strict';
+(function($) {
+    $(document).ready(function() {
+        $('.secondary li').on('mouseenter', function() {
+            $(this).find('.thirdly').slideDown();
+            $(this).on('mouseleave', function() {
+                $(this).find('.thirdly').slideUp();
+            });
+        });
+
+        $('.menu li').on('mouseenter', function(){
+        	var target = $(this).attr('data-target');
+        	$('.menu li').removeClass('active');
+        	$('.secondary-wrap[id!="' + target + '"]').slideUp();
+        	$(this).addClass('active');
+        	$('#' + target).slideDown();
+        });
+
+        $('.secondary-wrap').on('mouseleave', function(){
+        	var id = $(this).attr('id');
+        	var _this = $(this);
+        	$('.menu li').removeClass('active');
+        	$(this).slideUp();
+        	window.setTimeout(function(){
+
+        	}, 100);
+        	
+        });
+
+    });
+})(jQuery);
